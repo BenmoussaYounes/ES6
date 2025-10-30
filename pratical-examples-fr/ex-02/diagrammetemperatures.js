b238b50fc21e70c6dfeb7e9ad9cb04320c88d32a
younes <devbenmoussayounes@gmail.com>
2025-10-30 17:51:45 +0100

    feature(pratical-examples-fr) ✨ add second pratical example

---
+++ pratical-examples-fr/ex-02/diagrammetemperatures.js
@@ -0,0 +1,21 @@
+import {
+  Chart,
+  Colors,
+  LineController,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Legend
+} from './chartjsdist/chart.js';
+
+Chart.register(
+  Colors,
+  LineController,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Legend
+);
+
